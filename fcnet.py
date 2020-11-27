7e86f7dca6de83b5b6d9c24ccda9eab75bb8332c
Bread-Jin <55316494+Bread-Jin@users.noreply.github.com>
2020-11-27 20:47:30 +0900

    Update fcnet.py

--- fcnet.py
+++ fcnet.py
@@ -58,6 +58,22 @@ class netC1(nn.Module):
         nn.Conv1d(ndf, ndf, kernel_size=1, bias=False),
         nn.LeakyReLU(0.2),
         nn.Conv1d(ndf, ndf, kernel_size=1, bias=False),
+        nn.LeakyReLU(0.2),
+        nn.Conv1d(ndf, ndf, kernel_size=1, bias=False),
+        nn.LeakyReLU(0.2),
+        nn.Conv1d(ndf, ndf, kernel_size=1, bias=False),
+        nn.LeakyReLU(0.2),
+        nn.Conv1d(ndf, ndf, kernel_size=1, bias=False),
+        nn.LeakyReLU(0.2),
+        nn.Conv1d(ndf, ndf, kernel_size=1, bias=False),
+        nn.LeakyReLU(0.2),
+        nn.Conv1d(ndf, ndf, kernel_size=1, bias=False),
+        nn.LeakyReLU(0.2),
+        nn.Conv1d(ndf, ndf, kernel_size=1, bias=False),
+        nn.LeakyReLU(0.2),
+        nn.Conv1d(ndf, ndf, kernel_size=1, bias=False),
+        nn.LeakyReLU(0.2),
+        nn.Conv1d(ndf, ndf, kernel_size=1, bias=False),
         )
         """
        self.trunk = nn.Sequential(
